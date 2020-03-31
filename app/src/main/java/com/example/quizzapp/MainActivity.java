7e41fb014871bc2adad91ac6fbcc8a6eb4fcf0b6
Ibrahim <ibrahimalhefdhi@gmail.com>
2020-03-31 19:40:31 +0300

    v2

--- app/src/main/java/com/example/quizzapp/MainActivity.java
+++ app/src/main/java/com/example/quizzapp/MainActivity.java
@@ -2,81 +2,75 @@ package com.example.quizzapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private int scoreOf7=0;
-    private boolean q1=true;
-    private boolean q2=true;
-    private boolean q3=true;
-    private boolean q4=true;
-    private boolean q5=true;
-    private boolean q6=true;
-    private boolean q7=true;
+    private int scoreOf7 = 0;
+    private boolean EnterOncePerSubmtion = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void Submit(View view){
+
+    public void Submit(View view) {
         RadioButton qustion1 = (RadioButton) findViewById(R.id.q1_cho2);
         RadioButton qustion2 = (RadioButton) findViewById(R.id.q2_cho1);
-        CheckBox qustion3_1 = (CheckBox) findViewById(R.id.q3_cho1);
-        CheckBox qustion3_2 = (CheckBox) findViewById(R.id.q3_cho2);
-        CheckBox qustion3_3 = (CheckBox) findViewById(R.id.q3_cho3);
-        CheckBox qustion3_4 = (CheckBox) findViewById(R.id.q3_cho4);
+        CheckBox qustionThree1 = (CheckBox) findViewById(R.id.q3_cho1);
+        CheckBox qustionThree2 = (CheckBox) findViewById(R.id.q3_cho2);
+        CheckBox qustionThree3 = (CheckBox) findViewById(R.id.q3_cho3);
+        CheckBox qustionThree4 = (CheckBox) findViewById(R.id.q3_cho4);
         RadioButton qustion4 = (RadioButton) findViewById(R.id.q4_cho4);
         RadioButton qustion5 = (RadioButton) findViewById(R.id.q5_cho2);
         RadioButton qustion6 = (RadioButton) findViewById(R.id.q6_cho2);
-        EditText qustion7 = (EditText) findViewById(R.id.ana_q7);
-        if (q1){
-            cheak(qustion1.isChecked());
-            q1=false;
-        }
-        if (q2){
-            cheak(qustion2.isChecked());
-            q2=false;
-        }
-        if (q3){
-            cheak(qustion3_1.isChecked() && qustion3_2.isChecked() && !qustion3_3.isChecked() && !qustion3_4.isChecked());
-            q3=false;
-        }
-        if (q4){
-            cheak(qustion4.isChecked());
-            q4=false;
-        }
-        if (q5){
-            cheak(qustion5.isChecked());
-            q5=false;
-        }
-        if (q6){
-            cheak(qustion6.isChecked());
-            q6=false;
-        }
-        if(q7) {
-            if (qustion7.getText().toString().equalsIgnoreCase("object oriented"))
-                if (scoreOf7 < 7)
-                    scoreOf7++;
-                q7=false;
-        }
         TextView score = (TextView) findViewById(R.id.score);
         EditText name = (EditText) findViewById(R.id.name);
-        score.setText(name.getText().toString()+" Score is: " + scoreOf7);
+        EditText qustion7 = (EditText) findViewById(R.id.ana_q7);
+        if (EnterOncePerSubmtion) {
+            cheak(qustion1.isChecked());
+            cheak(qustion2.isChecked());
+            cheak(qustionThree1.isChecked() && qustionThree2.isChecked() && !qustionThree3.isChecked() && !qustionThree4.isChecked());
+            cheak(qustion4.isChecked());
+            cheak(qustion5.isChecked());
+            cheak(qustion6.isChecked());
+            if (qustion7.getText().toString().equalsIgnoreCase("object oriented")) {
+                if (scoreOf7 < 7)
+                    scoreOf7++;
+                else
+                    return;
+            }
+            EnterOncePerSubmtion = false;
+        } else
+            return;
+        String scoreWithname = name.getText().toString() + " Score is: " + scoreOf7;
+        score.setText(scoreWithname);
+        Context context = getApplicationContext();
+        CharSequence text = scoreWithname;
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
-    private void cheak(Boolean aBoolean){
+
+    private void cheak(Boolean aBoolean) {
         if (aBoolean)
-            if (scoreOf7 <7)
-            scoreOf7++;
+            if (scoreOf7 < 7)
+                scoreOf7++;
+            else
+                return;
     }
-    public void rest(View view){
-        scoreOf7=0;
+
+    public void rest(View view) {
+        scoreOf7 = 0;
         TextView score = (TextView) findViewById(R.id.score);
         score.setText("");
-        q1=q2=q3=q4=q5=q6=q7=true;
+        EnterOncePerSubmtion = true;
     }
 }
